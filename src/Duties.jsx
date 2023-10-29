62c36399679b45d670d40d5b96cadd05f83ac272
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-29 11:11:37 -0400

    add uuids

--- src/Duties.jsx
+++ src/Duties.jsx
@@ -1,5 +1,18 @@
+import { FaAngleDoubleRight } from 'react-icons/fa';
+import { v4 as uuidv4 } from 'uuid';
+
 const Duties = ({ duties }) => {
-  console.log(duties);
-  return <div>Duties</div>;
+  return (
+    <div>
+      {duties.map((duty, index) => {
+        return (
+          <div key={uuidv4()} className="job-desc">
+            <FaAngleDoubleRight className="job-icon" />
+            <p>{duty}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 export default Duties;
